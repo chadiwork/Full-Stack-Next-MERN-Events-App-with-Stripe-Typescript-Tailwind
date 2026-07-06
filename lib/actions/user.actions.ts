3f0753066a1cbcaed3bb9e6e023274895e4796f2
chadi.harda <chadi.harda@inkode.it>
2026-07-06 20:25:48 +0200

    User actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,18 @@
+'use server'
+
+import { CreateUserParams } from "@/types";
+import { connectToDatabase } from "../database";
+import { handleError } from "../utils";
+import User from "../database/model/user.model";
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+        await connectToDatabase();
+
+        const newUser  = await User.create(user);
+
+        return JSON.parse(JSON.stringify(newUser));
+    } catch (error) {
+        handleError(error)
+    }
+}
